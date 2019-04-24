34a45755be7f62290ad7a6daaac8e3148828b447
maimaiti <937765354@qq.com>
2019-04-24 15:46:56 +0800

    first Commit

---
+++ Classes/MsdSDK.h
@@ -0,0 +1,23 @@
+
+//
+//  PrivateSDK.h
+//  Pods
+//
+//  Created by MemoryFate on 2019/1/2.
+//
+
+#ifndef MsdSDK_h
+#define MsdSDK_h
+
+#import "UIColor+KYSSDK.h"
+#import "UIFont+KYSSDK.h"
+#import "UILabel+KYSSDK.h"
+#import "UITextField+KYSSDK.h"
+#import "UIView+KYSSDK.h"
+#import "Marco.h"
+#import "UIButton+KYSSDK.h"
+#import "UIView+frame.h"
+#import "baseAlertSelectView.h"
+#import "imagePiker.h"
+
+#endif /* MsdSDK_h */
